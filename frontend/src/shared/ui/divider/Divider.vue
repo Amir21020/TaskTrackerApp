2e9188166c7a1f333ecf74467e68289422c1ab3f
Amir Amirov <mergalievradmir484@gmail.com>
2026-05-02 15:15:41 +0300

    feat(shared): add reusable Divider ui component

---
+++ frontend/src/shared/ui/divider/Divider.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="relative flex items-center my-6">
+        <div class="flex-grow border-t border-gray-300"></div>
+        <span v-if="text" class="flex-shrink-0 mx-4 text-sm text-gray-500">
+            {{ text }}
+        </span>
+        <div class="flex-grow border-t border-gray-300"></div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    text: {
+        type: String,
+        default: '' 
+    }
+});
+</script>
--- frontend/src/shared/ui/index.js
+++ frontend/src/shared/ui/index.js
@@ -1,3 +1,4 @@
 export { default as BaseButton } from './base-button/BaseButton.vue'
 export { default as BaseInput } from './base-input/BaseInput.vue'
 export { default as PasswordInput } from './password-input/PasswordInput.vue'
+export  { default as Divider } from './divider/Divider.vue'
