0e31cc45dac17b986bc5096c854709c88cb3b189
Amir Amirov <mergalievradmir484@gmail.com>
2026-05-03 15:58:39 +0300

    feat(ui): add BaseCheckbox component

---
+++ frontend/src/shared/ui/base-checkbox/BaseCheckbox.vue
@@ -0,0 +1,21 @@
+<script setup>
+const model = defineModel()
+defineProps({
+    label: String
+})
+
+
+</script>
+
+<template>
+    <div>
+        <label class="flex items-center cursor-pointer">
+            <input
+                type="checkbox"
+                v-model="model"
+                class="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+            />
+            <span class="ml-2 text-sm text-gray-700">{{ label }}</span>
+        </label>
+    </div>
+</template>
